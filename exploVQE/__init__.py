c074a45406e0022434397cc576aafcd718cc24bc
MarcoSciorilli <sciorilli.marco@gmail.com>
2022-03-17 15:08:44 +0400

    Changes by marco

--- exploVQE/__init__.py
+++
@@ -1,6 +0,0 @@
-from exploVQE import plots
-from exploVQE import ansatz
-from exploVQE import newgraph
-from exploVQE import dataretriver
-from exploVQE import resultevaluater
-from exploVQE import multibase
---
+++ multiVQA/__init__.py
@@ -0,0 +1,6 @@
+from multiVQA import plots
+from multiVQA import ansatz
+from multiVQA import newgraph
+from multiVQA import dataretriver
+from multiVQA import resultevaluater
+from multiVQA import multibase
